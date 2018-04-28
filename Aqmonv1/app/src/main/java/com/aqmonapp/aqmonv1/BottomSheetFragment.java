d3df2133f32a2281bd31f4cad1863d154d36e356
Storytellerr <shashanksharma580S@gmail.com>
2018-04-28 08:25:05 +0530

    Bottom Sheet added

--- Aqmonv1/app/src/main/java/com/aqmonapp/aqmonv1/BottomSheetFragment.java
+++ Aqmonv1/app/src/main/java/com/aqmonapp/aqmonv1/BottomSheetFragment.java
@@ -1,17 +1,29 @@
 package com.aqmonapp.aqmonv1;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import devlight.io.library.ArcProgressStackView;
 
@@ -28,25 +40,76 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
 
     int bgColors[]={0xffffff00,0xff0000ff,0xffffff00,0xff0000ff};
     int mStartColors[]={0xffff00ff};
+    ProgressBar progrss ;
+    TextView tv;
+    ArcProgressStackView arcProgressStackView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
        View v= inflater.inflate(R.layout.fragment_bottom_sheet, container, false);
-       TextView tv=(TextView)v.findViewById(R.id.name);
+        tv=(TextView)v.findViewById(R.id.name);
         Bundle mArgs = getArguments();
         String name = mArgs.getString("name");
-       // String distance = mArgs.getString("distance");
+        arcProgressStackView = (ArcProgressStackView) v.findViewById(R.id.apsv);
+        progrss = (ProgressBar) v.findViewById(R.id.progressbar_bottomsheet);
         tv.setText(name);
-        final ArrayList<ArcProgressStackView.Model> models = new ArrayList<>();
-        models.add(new ArcProgressStackView.Model("Circle", 25, bgColors[0], mStartColors[0]));
-        models.add(new ArcProgressStackView.Model("Progress", 50, bgColors[1], mStartColors[0]));
-        models.add(new ArcProgressStackView.Model("Stack", 75, bgColors[2], mStartColors[0]));
-        models.add(new ArcProgressStackView.Model("View", 100, bgColors[3], mStartColors[0]));
 
-        final ArcProgressStackView arcProgressStackView = (ArcProgressStackView) v.findViewById(R.id.apsv);
-        arcProgressStackView.setModels(models);
+        // String distance = mArgs.getString("distance");
+
         return v;
+    }
+
+    public  class datafetch extends AsyncTask<String,Void,Map<String,Integer>>
+    {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected void onPostExecute(Map<String,Integer> map) {
+            super.onPostExecute(map);
+
+            final ArrayList<ArcProgressStackView.Model> models = new ArrayList<>();
+            Set<String> key = map.keySet();
+            Iterator<String> value = key.iterator();
+
+            while (value.hasNext()) {
+                String pollutant = value.next();
+                int amount = map.get(pollutant);
+                models.add(new ArcProgressStackView.Model(pollutant,amount, bgColors[1], mStartColors[0]));
+                Log.d(pollutant, " => " + amount);
+            }
+            arcProgressStackView.setModels(models);
+        }
+
+        @Override
+        protected Map<String,Integer> doInBackground(String... params) {
+            try {
+                URL url = new URL("");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                int statusCode = urlConnection.getResponseCode();
+                if (statusCode == 200) {
+                    InputStream it = new BufferedInputStream(urlConnection.getInputStream());
+                    InputStreamReader read = new InputStreamReader(it);
+                    BufferedReader buff = new BufferedReader(read);
+                    StringBuilder data = new StringBuilder();
+                    String chunks;
+                    while ((chunks = buff.readLine()) != null) {
+                        data.append(chunks);
+                    }
+                } else {
+                    //Handle else
+                }
+            }catch (Exception e){
+                
+            }
+            return null;
+        }
     }
 
 }
